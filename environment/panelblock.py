f2681125cea3db0bb51d8659e04da011cd233019
Youngin-Cho <56374406+Youngin-Cho@users.noreply.github.com>
2021-08-11 12:02:15 +0900

    data save

--- environment/panelblock.py
+++ environment/panelblock.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 import pandas as pd
@@ -31,6 +32,32 @@ def generate_block_data(num_of_process=6, num_of_blocks=50, size=1, distribution
     return process_time
 
 
+def write_block_date(filepath, num_of_process=6, num_of_blocks=40, case=30, distribution="lognormal"):
+
+    if distribution == "lognormal":
+        shape = [0.543, 0.525, 0.196, 0.451, 0.581, 0.432]
+        scale = [2.18, 2.18, 0.518, 2.06, 1.79, 2.10]
+    elif distribution == "uniform":
+        loc = [0 for _ in range(num_of_process)]
+        scale = [10 for _ in range(num_of_process)]
+
+    process_time = {}
+    for i in range(case):
+        process_time_temp = np.zeros((num_of_blocks, num_of_process))
+        for j in range(num_of_process):
+            if distribution == "lognormal":
+                r = np.round(stats.lognorm.rvs(shape[j], loc=0, scale=scale[j], size=num_of_blocks), 1)
+            elif distribution == "uniform":
+                r = np.round(stats.uniform.rvs(loc=loc[j], scale=scale[j], size=num_of_blocks), 1)
+            process_time_temp[:, j] = r
+        process_time["sheet_%d" % i] = pd.DataFrame(process_time_temp)
+
+    writer = pd.ExcelWriter(filepath + '/PBS_%d_%d.xlsx' % (num_of_process, num_of_blocks), engine='openpyxl')
+    for key, value in process_time.items():
+        value.to_excel(writer, sheet_name=key, index=False)
+    writer.save()
+
+
 def read_block_data(filepath):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     process_time = {}
@@ -43,5 +70,9 @@ def read_block_data(filepath):
 
 
 if __name__ == "__main__":
-    blocks = generate_block_data()
-    print("f")
+    file_dir = "./data"
+
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+
+    write_block_date(file_dir)
